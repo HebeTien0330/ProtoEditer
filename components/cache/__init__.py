3f52c2cf23d3ae9f7a7d380123437780772fce92
tangchengqin <527094604@qq.com>
2025-01-10 17:21:23 +0800

    #add cache

---
+++ components/cache/__init__.py
@@ -0,0 +1,9 @@
+'''
+:@Author: tangchengqin
+:@Date: 2025/1/10 17:10:57
+:@LastEditors: tangchengqin
+:@LastEditTime: 2025/1/10 17:10:57
+:Description: 
+:Copyright: Copyright (©) 2025 Clarify. All rights reserved.
+'''
+from .cache import getCache, query, save, update
